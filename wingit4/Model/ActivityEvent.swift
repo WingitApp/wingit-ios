c68f5c2d8199f3a707c9d471411ed259a3216cb8
Daniel Yee <danielyee517@gmail.com>
2021-08-31 00:45:29 -0700

    Add ActivityEvent model

--- wingit4/Model/ActivityEvent.swift
+++ wingit4/Model/ActivityEvent.swift
@@ -2,7 +2,25 @@
 //  ActivityEvent.swift
 //  wingit4
 //
-//  Created by (310) 748-1434 on 8/30/21.
+//  Created by Daniel Yee on 8/30/21.
 //
 
+import FirebaseFirestoreSwift
 import Foundation
+
+struct ActivityEvent: Codable, Identifiable {
+    @DocumentID var id: String?
+    var connectionId: String
+    var createdAt: Double
+    var mediaUrl: String
+    var text: String
+    var type: EventType
+    var userId: String
+}
+
+enum EventType: String, Codable {
+    case comment
+    case connectRequest
+    case connectRequestAccepted
+    case like
+}
