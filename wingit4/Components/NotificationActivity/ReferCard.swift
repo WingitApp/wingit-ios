76b56d5b02cb05c9567baddbb819f29a7c48d13d
Amy Chun <amyrimchun@gmail.com>
2021-08-31 22:53:03 -0700

    ReferConnectionList

---
+++ wingit4/Components/NotificationActivity/ReferCard.swift
@@ -0,0 +1,23 @@
+//
+//  ReferCard.swift
+//  wingit4
+//
+//  Created by YaeRim Amy Chun on 8/31/21.
+//
+
+import SwiftUI
+
+struct ReferCard: View {
+    var body: some View {
+        VStack{
+            Text("Hello, World!")
+        }
+     
+    }
+}
+
+struct ReferCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ReferCard()
+    }
+}
