78004e562dd87c3cba1e90a7ede0f979fe8431b0
Amy Chun <amyrimchun@gmail.com>
2021-08-31 22:46:11 -0700

    ConnectionsList

---
+++ wingit4/Components/Ask/ReferConnectionsList.swift
@@ -0,0 +1,20 @@
+//
+//  ReferConnectionsList.swift
+//  wingit4
+//
+//  Created by YaeRim Amy Chun on 8/31/21.
+//
+
+import SwiftUI
+
+struct ReferConnectionsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReferConnectionsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ReferConnectionsList()
+    }
+}
