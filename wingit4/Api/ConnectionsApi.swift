c33e33e303bf042e63e5caeb9b8a35ce3a603e65
Daniel Yee <danielyee517@gmail.com>
2021-09-02 00:34:52 -0700

    Move files to the right folders

---
+++ wingit4/Api/ConnectionsApi.swift
@@ -0,0 +1,31 @@
+//
+//  ConnectionsApi.swift
+//  wingit4
+//
+//  Created by Daniel Yee on 8/10/21.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseStorage
+import Firebase
+
+class ConnectionsApi {
+    func getConnections(userId: String, onSuccess: @escaping(_ users: [User]) -> Void) {
+        Ref.FS_COLLECTION_CONNECTIONS_FOR_USER(userId: userId).getDocuments { (snapshot, error) in
+            guard let snap = snapshot else {
+                return
+            }
+            var users = [User]()
+            for connection in snap.documents {
+                let connectionId = connection.documentID
+                Ref.FS_DOC_USERID(userId: connectionId).getDocument{ (document, error) in
+                    if let decodedUser = try? document?.data(as: User.self) {
+                        users.append(decodedUser)
+                    }
+                    onSuccess(users)
+                }
+            }
+        }
+    }
+}
---
+++ wingit4/Screens/Profile/ConnectionsView.swift
@@ -0,0 +1,44 @@
+//
+//  ConnectionsView.swift
+//  wingit4
+//
+//  Created by Daniel Yee on 8/10/21.
+//
+
+import SwiftUI
+import Firebase
+import URLImage
+
+struct ConnectionsView: View {
+    @EnvironmentObject var connectionsViewModel: ConnectionsViewModel
+    var user: User
+   
+    var body: some View {
+        VStack {
+            List {
+                ForEach(self.connectionsViewModel.users, id: \.uid) { user in
+                        NavigationLink(destination: UserProfileView(user: user)) {
+                            HStack {
+                            URLImage(URL(string: user.profileImageUrl)!,
+                            content: {
+                                $0.image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipShape(Circle())
+                            }).frame(width: 50, height: 50)
+                                
+                                VStack(alignment: .leading, spacing: 5) {
+                                 Text(user.username).font(.headline).bold()
+                                    Text(user.bio).font(.subheadline)
+                                }
+                            }.padding(10)
+                   }
+                }
+            }
+        }
+        .navigationBarTitle(Text("Connections"), displayMode: .inline)
+        .onAppear {
+            connectionsViewModel.loadConnections(userId: user.id)
+        }
+    }
+}
