dcdb9046f950b8e16825cabaf8bd437985f49bf2
Daniel Yee <danielyee517@gmail.com>
2021-09-13 04:33:23 -0700

    Remove duplicate setData() for posts

--- wingit4/Api/StorageService.swift
+++ wingit4/Api/StorageService.swift
@@ -64,7 +64,6 @@ class StorageService {
                     do {
                         try firestorePostRef.setData(from: post)
                         try Ref.FS_DOC_TIMELINE_FOR_USERID(userId: userId).collection("timelinePosts").document(postId).setData(from: post)
-                        try Ref.FS_COLLECTION_ALL_POSTS.document(postId).setData(from: post)
                         onSuccess()
                     } catch {
                         print(error)
