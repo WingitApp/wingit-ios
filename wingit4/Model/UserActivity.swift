679e7d34c481f7bc7d89ebe3959fd7c2c882704d
Daniel Yee <danielyee517@gmail.com>
2021-09-28 23:14:41 -0700

    Improve UserActivity fields

--- wingit4/Model/UserActivity.swift
+++ wingit4/Model/UserActivity.swift
@@ -12,29 +12,28 @@ import Foundation
 struct UserActivity: Codable, Identifiable {
     @DocumentID var id: String?
     @ServerTimestamp var createdAt: Timestamp?
+    var activityType: ActivityType?
     var commentId: String?
-    var commentOwnerId: String? // id of the user who owns the comment
-    var connectionId: String? // id of the user related to this connect request or connection activity
+    var correspondingUserDisplayName: String?
+    var correspondingUserId: String?
     var currentUserDisplayName: String? // display name of the currentUser performing the activity
     var currentUserId: String? // id of the currentUser performing the activity
     var mediaUrl: String?
-    var otherUsersDisplayName: String? // displayName of the user whom the currentUser is interacting with
-    var postOwnerId: String? // id of the user who owns the post related to this activity
-    var postId: String?
-    var recipientId: String?
-    var senderId: String? // id of the user winging the ask or sending the message
+    var postOwnerId: String? // id of the user who owns the post IF this activity relates to a post
+    var postId: String? // id of the post IF this activity relates to a post
+    var postTitle: String?
+    var postType: PostType?
     var text: String? // text found in the comment, message, referral, post, etc.
-    var type: ActivityType
     
     enum ActivityType: String, Codable {
         case acceptConnectRequest
         case declineConnectRequest
         case declineReferral
         case followPost
-        case reactToComment
-        case referConnection
         case postAsk
         case postComment
+        case reactToComment
+        case referConnection
         case searchForUser
         case sendConnectRequest
         case sendMessage
