e9fac308cee64d546d925968fa54a88bec4bc0a1
joshslee <joshslee93@gmail.com>
2021-10-02 15:21:53 -0700

    add ui components

---
+++ wingit4/UIKit/BackgroundBlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundBlurView.swift
+//  wingit4
+//
+//  Created by Joshua Lee on 9/30/21.
+//
+
+import SwiftUI
+
+struct BackgroundBlurView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
