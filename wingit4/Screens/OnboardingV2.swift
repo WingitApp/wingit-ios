edbc60d82ec42b0d2a5c3845726d5343d5eb9417
joshslee <joshslee93@gmail.com>
2021-10-14 15:03:41 -0700

    rebase and fix projectfile errors. add session pending state for onboardingV2

--- wingit4/Screens/OnboardingV2.swift
+++ wingit4/Screens/OnboardingV2.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct OnboardingV2: View {
-  
+  @EnvironmentObject var session: SessionStore
   @State var offset: CGFloat = 0
   @State var screenIndex: CGFloat = 0  
   
@@ -31,102 +31,103 @@ struct OnboardingV2: View {
   }
   
   var body: some View {
+    ActivityIndicatorView(message: "Loading...", isShowing: self.$session.isSessionLoading) {
+      OffsetPageTabView(offset: $offset) {
+        
+        HStack(spacing: 0){
     
-    // Custom Pager View...
-    OffsetPageTabView(offset: $offset) {
-      
-      HStack(spacing: 0){
-  
-        ForEach(boardingScreens.indices){ index in
-          
-          VStack(spacing: 15){
+          ForEach(boardingScreens.indices){ index in
             
-            Image(boardingScreens[index].image ?? "")
-              .resizable()
-              .aspectRatio(contentMode: .fit)
-              .frame(width: getScreenBounds().width - 100, height: getScreenBounds().width - 100)
-              .scaleEffect(getScreenBounds().height < 750 ? 0.9 : 1)
-              .offset(y: getScreenBounds().height < 750 ? -100 : -120)
+            VStack(spacing: 15){
+              
+              Image(boardingScreens[index].image ?? "")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: getScreenBounds().width - 100, height: getScreenBounds().width - 100)
+                .scaleEffect(getScreenBounds().height < 750 ? 0.9 : 1)
+                .offset(y: getScreenBounds().height < 750 ? -100 : -120)
+              
+              OnboardText(screen: boardingScreens[index])
+            }
+            .padding()
+            .frame(width: getScreenBounds().width)
+            .frame(maxHeight: .infinity)
             
-            OnboardText(screen: boardingScreens[index])
+            if index == boardingScreens.count - 1 {
+              OnboardingView()
+            }
           }
+        }
+      }
+      .background(
+        
+        RoundedRectangle(cornerRadius: 50)
+          .fill(.white)
+        // Size as image size...
+          .frame(width: getScreenBounds().width - 100,height: getScreenBounds().width - 100)
+          .scaleEffect(2)
+          .rotationEffect(.init(degrees: 25))
+          .rotationEffect(.init(degrees: getRotation()))
+          .offset(y: -getScreenBounds().width + 20)
+        
+        ,alignment: .leading
+      )
+      .background(
+        Color("screen\(getIndex() + 1)")
+          .animation(.easeInOut, value: getIndex())
+      )
+      // animating...
+      .ignoresSafeArea(.container, edges: .all)
+      .overlay(
+        VStack{
+          HStack{
+            Button {
+              if getIndex() > 0 {
+                offset = min(offset - getScreenBounds().width,getScreenBounds().width * 3)
+              }
+            } label: {
+              Text("Back")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            }
+            
+            .opacity(getIndex() > 0 ? 1 : 0)
+            .allowsHitTesting(getIndex() > 0)
+            .frame(width: UIScreen.main.bounds.width / 3.5)
+            
+            
+            HStack(spacing: 8){
+              ForEach(boardingScreens.indices,id: \.self){index in
+                Circle()
+                  .fill(.white)
+                  .opacity(index == getIndex() ? 1 : 0.4)
+                  .frame(width: 8, height: 8)
+                  .scaleEffect(index == (getIndex()) ? 1.3 : 0.85)
+                  .animation(.easeInOut, value: getIndex())
+              }
+            }
+            .frame(maxWidth: .infinity)
+            
+            Button {
+              offset = min(offset + getScreenBounds().width,getScreenBounds().width * 3)
+            } label: {
+              Text(getIndex() == 2 ? "Get Started" : "Next")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            }
+            .frame(width: UIScreen.main.bounds.width/3.5)
+            
+          }
+          .padding(.top, 30)
+          .padding(.horizontal, 8)
+        }
           .padding()
-          .frame(width: getScreenBounds().width)
-          .frame(maxHeight: .infinity)
-          
-          if index == boardingScreens.count - 1 {
-            OnboardingView()
-          }
-        }
-      }
+        ,
+        alignment: .bottom
+      )
     }
-    
-    .background(
-      
-      RoundedRectangle(cornerRadius: 50)
-        .fill(.white)
-      // Size as image size...
-        .frame(width: getScreenBounds().width - 100,height: getScreenBounds().width - 100)
-        .scaleEffect(2)
-        .rotationEffect(.init(degrees: 25))
-        .rotationEffect(.init(degrees: getRotation()))
-        .offset(y: -getScreenBounds().width + 20)
-      
-      ,alignment: .leading
-    )
-    .background(
-      Color("screen\(getIndex() + 1)")
-        .animation(.easeInOut, value: getIndex())
-    )
-    // animating...
-    .ignoresSafeArea(.container, edges: .all)
-    .overlay(
-      VStack{
-        HStack{
-          Button {
-            if getIndex() > 0 {
-              offset = min(offset - getScreenBounds().width,getScreenBounds().width * 3)
-            }
-          } label: {
-            Text("Back")
-              .fontWeight(.semibold)
-              .foregroundColor(.white)
-          }
-          
-          .opacity(getIndex() > 0 ? 1 : 0)
-          .allowsHitTesting(getIndex() > 0)
-          .frame(width: UIScreen.main.bounds.width / 3.5)
-          
-          
-          HStack(spacing: 8){
-            ForEach(boardingScreens.indices,id: \.self){index in
-              Circle()
-                .fill(.white)
-                .opacity(index == getIndex() ? 1 : 0.4)
-                .frame(width: 8, height: 8)
-                .scaleEffect(index == (getIndex()) ? 1.3 : 0.85)
-                .animation(.easeInOut, value: getIndex())
-            }
-          }
-          .frame(maxWidth: .infinity)
-          
-          Button {
-            offset = min(offset + getScreenBounds().width,getScreenBounds().width * 3)
-          } label: {
-            Text(getIndex() == 2 ? "Get Started" : "Next")
-              .fontWeight(.semibold)
-              .foregroundColor(.white)
-          }
-          .frame(width: UIScreen.main.bounds.width/3.5)
-          
-        }
-        .padding(.top, 30)
-        .padding(.horizontal, 8)
-      }
-        .padding()
-      ,
-      alignment: .bottom
-    )
+    // Custom Pager View...
+   
   }
   
 }
