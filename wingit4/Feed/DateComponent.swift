866f7d649b30ef4c7f60b7ada3e8502053fc12c5
yrac34 <amyrimchun@gmail.com>
2021-11-04 10:25:29 -0700

    Fixed Minor mistakes

---
+++ wingit4/Feed/DateComponent.swift
@@ -0,0 +1,21 @@
+//
+//  DateComponent.swift
+//  wingit4
+//
+//  Created by Amy Chun on 11/4/21.
+//
+
+import SwiftUI
+
+struct DateComponent: View {
+  
+  var date: String = "March 3, 0000"
+    var body: some View {
+      Text(date)
+        .font(.caption)
+        .bold()
+        .padding(.leading, 5)
+    }
+}
+
+
